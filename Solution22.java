54c8ac3318fefc72732fc212a9fa05f1574603fb
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:40:56 +0530

    Create Solution22.java

---
+++ Solution22.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution22 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2!=0)
+            System.out.println("ODD");
+        else
+            System.out.println("EVEN");
+    }
+}
