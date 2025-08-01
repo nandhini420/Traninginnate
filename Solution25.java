f7b22dc27564178a54f4946288833689f1aaea73
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:45:09 +0530

    Create Solution25.java

---
+++ Solution25.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+//import java.util.Scanner;
+
+public class Solution25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%15 == 0) {
+            System.out.println("HiHello");
+        } else if (n%3 == 0) {
+            System.out.println("Hi");
+        } else {
+            System.out.println("Hello");
+        }
+    }
+}
