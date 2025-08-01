b59a844ff30c41eec67fe70355a7e0f2b8df6f45
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:28:00 +0530

    Create Solution7.java

---
+++ Solution7.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution7 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long r = 0,rem = 0,copy = n;
+        
+        while(n> 0){
+            r = n%10;
+            n = n/10;
+            rem = rem * 10 + r;
+        }
+        if (copy == rem)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
