be40604ff9dd9a4ea5289c322a96dfe2114a4d86
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:32:11 +0530

    Update and rename Solution2.java to Solution32.java

--- Solution32.java
+++ Solution32.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution2 {
+public class Solution32 {
      public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
