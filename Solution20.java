0b1f65b213e49b295ba44a597671dc25efb64c75
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:48:28 +0530

    Create Solution20.java

---
+++ Solution20.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+//import java.util.Scanner;
+
+public class Solution20{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int monthNumber = scanner.nextInt();
+        scanner.close();
+
+        String[] months = {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+
+        if (monthNumber >= 1 && monthNumber <= 12) {
+            System.out.println(months[monthNumber - 1]);
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+}
