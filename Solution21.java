7cdd01c3ea84dc8e9b8858eff23dc1aeca5306ee
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:40:29 +0530

    Create Solution21.java

---
+++ Solution21.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution21 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if ( a== b)
+            System.out.println("EQUAL");
+        else
+            System.out.println("NOT EQUAL");
+    }
+}
