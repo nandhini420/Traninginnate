02757ba5bfa455018852c3fa8fb1d3ad1aae9807
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:24:48 +0530

    Create Solutionm43.java

---
+++ Solutionm43.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solutionm43 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long ld;
+        long sum = 1;
+
+        while (n > 0) {
+           ld = n % 10;
+            sum=sum * ld ;
+            n = n / 10;
+        }
+        
+        System.out.println(sum);
+    }
+}
