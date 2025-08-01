0c4f69c6a3e06ca83d5584681049881e0320e25f
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:38:26 +0530

    Create Solution42.java

---
+++ Solution42.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution42 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long ld;
+        long n = sc.nextLong();
+        int a = sc.nextInt();
+        int count = 0;
+
+        while (n > 0) {
+             ld = n % 10 ;
+            n = n / 10 ;
+            if ( ld == a)
+            {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
