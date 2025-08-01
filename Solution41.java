579578ae126320bae3778b883998ec85b2b511a8
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:37:46 +0530

    Create Solution41.java

---
+++ Solution41.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution41 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        int count = 0;
+
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+        
+        System.out.println(count);
+    }
+}
