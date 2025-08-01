df8e0c24761e36120533bcfe4906bd4fba0d2169
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:28:37 +0530

    Create Solution8.java

---
+++ Solution8.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class Solution8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long square = n * n;
+        long r = 0, ra =0,rem = 0,result = 0 , copy = n;
+        while(n> 0){
+            r = n%10;
+            n = n/10;
+            rem = rem * 10 + r;
+        }
+         while(square > 0){
+            ra = square %10;
+            square = square /10;
+            result = result * 10 + ra;
+        }
+        long remsquare = rem * rem ;
+        if(result == remsquare)
+            System.out.println("Adam Number");
+        else
+            System.out.println("Not a adam number");
+    }
+}
