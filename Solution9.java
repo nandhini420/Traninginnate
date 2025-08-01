20a56c97c541413303227224fe33e3af6e1e5a03
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:29:45 +0530

    Create Solution9.java

---
+++ Solution9.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution9 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            int copy = num;
+            int result = 0;
+            int n = num;
+            int count = 0;
+                while(n >0){
+                        count ++;
+                        n = n/10;
+                        
+                }
+            while (num > 0) {
+            int digit = num % 10;
+            result += Math.pow(digit, count);
+            num /= 10;
+        }
+
+        if (result == copy)
+            System.out.println("Armstrong number");
+        else
+            System.out.println("Not an Armstrong number");          
+    }
+}
