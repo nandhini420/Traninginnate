3a34278dbd30e56f1b4fe4d240c295a6470170a1
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 18:25:22 +0530

    Update and rename Solution28.java to ValidateDateFormat.java

--- ValidateDateFormat.java
+++ ValidateDateFormat.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Solution28 {
+public class ValidateDateFormat{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String timeString = sc.next();
