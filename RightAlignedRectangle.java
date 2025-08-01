5211976a9f020278de40bc9d5bdc485fa0a60486
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 18:13:52 +0530

    Update and rename Solution54.java to RightAlignedRectangle.java

--- RightAlignedRectangle.java
+++ RightAlignedRectangle.java
@@ -4,7 +4,9 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution54 {
+public class RightAlignedRectangle
+
+{
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
