e83237ceeaf6270ddbc68aea53e2009ad8d2260f
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:25:25 +0530

    Create Solution4.java

---
+++ Solution4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            long n = sc.nextLong();
+            long ld;
+        long copy = n;
+        long p=1;
+        while(n>0){
+            n = n/10;
+            p = p * 10;
+        }
+        p=p/10;
+        n= copy;
+            while (p > 0) {
+                ld = n / p;  
+                n = n % p ;
+                p = p/10;
+
+                if (ld == 2 || ld == 3 || ld == 5 || ld == 7) {
+                    System.out.print(ld + " ");
+                }
+            }
+        }
+    }
+
