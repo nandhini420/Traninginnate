3ddcc3dcd5703841f9754689c38d377fe222d080
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:34:30 +0530

    Update and rename Solution4.java to Solution44.java

--- Solution44.java
+++ Solution44.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Solution4 {
+public class Solution44 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             long n = sc.nextLong();
