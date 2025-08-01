13b81396f8ecbb5b37e49e8795e234d725bdc43d
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-01 11:15:24 +0530

    Create Solution1.java

---
+++ Solution1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution1 {
+ public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+       for(int i = 1;i <= a ;i++){
+           if(i%2!=0){
+               System.out.print(i+" ");
+           }
+       }
+}
+}
