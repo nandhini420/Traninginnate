eed7bfda9b54629d8d237c51210b92b55636f2ae
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-07 12:28:20 +0530

    Create ArraySearch.java

---
+++ ArraySearch.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ArraySearch{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+     int []n = new int[a];
+        for(int i = 0;i < a;i++){
+            n[i]=sc.nextInt();
+        }
+        int data = sc.nextInt();
+        boolean flag = false;
+        for(int i = 0;i < a;i++){
+            if(n[i]==data){
+                System.out.println("Found");
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false){
+         System.out.println("Not found");
+        }
+    }
+}
