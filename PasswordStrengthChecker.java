92215bf7021f6c1c508737d7d59c1d4cadd04109
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-16 10:54:23 +0530

    Create PasswordStrengthChecker.java

---
+++ PasswordStrengthChecker.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class PasswordStrengthChecker {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc = new Scanner(System.in);
+            String password = sc.nextLine();
+            int n = password.length();
+            int u = 0;
+            int l = 0;
+            int d = 0;
+            int s = 0;
+            if(n>=10){
+            for(int i = 0;i<n;i++){
+                    char ch = password.charAt(i);
+                    if(ch>='A' && ch <= 'Z')
+                            u = 1;
+                    if(ch>='a' && ch <= 'z')
+                            l = 1;
+                    if(ch>='0' && ch <= '9')
+                            d = 1;
+                    else
+                            s = 1;
+                    
+            }
+            }
+            if(u+l+d+s == 4){
+                    System.out.println("Strong password");
+            }
+            else{
+                    System.out.println("Weak password");
+            }
+    }
+}
