3aa7ed5f92b0a95129f06cabce31bbbbbab11fc6
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-16 11:45:04 +0530

    Create ParenthesesBalancer.java

---
+++ ParenthesesBalancer.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ParenthesesBalancer {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc = new Scanner(System.in);
+            String word = sc.nextLine();
+            int n = word.length();
+            int count = 0;
+            boolean balanced = true;
+            for(int i = 0;i<n;i++){
+                    char ch = word.charAt(i);
+                    if(ch == '('){
+                            count++;
+                    }
+                    else if(ch == ')'){
+                            count--;
+                    
+                    if (count < 0) { 
+                    balanced = false;
+                    break;
+                }
+            }
+    }
+             if (balanced && count == 0) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Unbalanced");
+        }
+            
+}
+}
