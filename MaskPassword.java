987b5ab7ef50504d4758c1c673d9edfb58b93e4c
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-25 19:08:24 +0530

    Create MaskPassword.java

---
+++ MaskPassword.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class MaskPassword{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        String pass = "";
+        for(int i =0 ;i<word.length();i++){
+           char ch = '*';
+            pass += ch;
+        }
+        System.out.println(pass);
+    }
+}
