7ef80f8f3b882fcaa749eb17a95ee3aba5cb1c2c
Naga Nandhini P <nandhini4205@gmail.com>
2025-08-31 19:02:07 +0530

    Create Collatzproblem.java

---
+++ Java/Collatzproblem.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Collatzproblem{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int count=0;
+            System.out.println(n);
+            while(n!=1){
+            if(n%2==0){
+                    n = n/2;
+            }
+            else{
+                    n = (3*n)+1;
+            }
+            System.out.println(n);
+            count++;
+            }
+            System.out.println(count);
+    }
+}
